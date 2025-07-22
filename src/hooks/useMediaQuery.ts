8be822fde0ebbfc2662fb39a744e62668f6f6865
Iamshub25 <shubhampra25@gmail.com>
2025-07-22 20:02:16 +0530

    removed comment

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -4,20 +4,12 @@ export function useMediaQuery(query: string): boolean {
   const [matches, setMatches] = useState(false);
   
   useEffect(() => {
-    // Check if window is defined (so if in the browser or in SSR)
+    
     if (typeof window !== 'undefined') {
       const media = window.matchMedia(query);
-      
-      // Set initial value
       setMatches(media.matches);
-      
-      // Define a callback function to handle changes
       const listener = () => setMatches(media.matches);
-      
-      // Add the callback function as a listener for changes to the media query
       media.addEventListener('change', listener);
-      
-      // Remove the listener when the component is unmounted
       return () => {
         media.removeEventListener('change', listener);
       };
